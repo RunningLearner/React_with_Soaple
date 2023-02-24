4732a0243d22f9eb8eeb9f7c23f148b811e60b28
RunningLearner <tkacnsdms@naver.com>
2023-02-24 16:56:33 +0900

    feat: 생명주기 함수가 언제 작동하는지 확인해보기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,13 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Clock from "./chapter_04/Clock";
 import CommentList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
 
 // import Library from "./chapter_03/Library";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    {/* <CommentList /> */}
+    <NotificationList />
   </React.StrictMode>
 );
 
